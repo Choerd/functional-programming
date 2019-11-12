4399e6ed46a0b6df825014d1fa68aaa8e79812be
Sjors <sjorspieter@live.nl>
2019-11-12 19:19:34 +0100

    d3-ready

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const endpoint =
-    "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-24/sparql";
+    "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-24/sparql"
 
 const sparql = `
 PREFIX dc: <http://purl.org/dc/elements/1.1/>
@@ -17,7 +17,7 @@ SELECT  ?cho
 		?landLabel
 WHERE {
 <https://hdl.handle.net/20.500.11840/termmaster2809> skos:narrower ?cat . # keuze maken in categorie
-  
+
   # variabelen definiëren
   ?cho edm:isRelatedTo ?cat . # ophalen van de link/uri van het voorwerp
   ?cho dct:medium ?materiaal . # ophalen van het materiaal van het voorwerp
@@ -27,27 +27,33 @@ WHERE {
   ?plaats skos:exactMatch/wgs84:lat ?lat . #
   ?plaats skos:exactMatch/wgs84:long ?long .
   ?plaats skos:exactMatch/gn:parentCountry ?land .
-      
+
   #Weergeven van de URI's in tekst
   ?cho dc:title ?title . # titel van het voorwerp
   ?cat skos:prefLabel ?catName .
   ?land gn:name ?landLabel .
   ?materiaal skos:prefLabel ?materiaalLabel .
-  ?cat skos:prefLabel ?catName .
 
 } ORDER BY DESC(?cho)
-`;
+`
+
+fetchData()
+    .then(data => fixReadability(data))
+    .then(readableData => amoutOfEachMaterial(readableData))
+    .then(allMaterials => fiveMostOccuringMaterials(allMaterials))
+    .then(iets => console.log(iets))
 
 async function fetchData() {
-    // Ophalen van de data en omzetten naar json
-    const response = await fetch(endpoint + "?query=" + encodeURIComponent(sparql) + "&format=json");
-    const data = await response.json();
-    const opgehaaldeDataArray = data.results.bindings
+    const response = await fetch(endpoint + "?query=" + encodeURIComponent(sparql) + "&format=json")
+    const data = await response.json()
 
-    // Opgehaalde JSON aanpassen voor een betere leesbaarheid
-    const aangepasteDataArray = opgehaaldeDataArray.map(async (result, index) => {
+    return data.results.bindings
+}
+
+function fixReadability(data) {
+    return data.map((result, index) => {
         return {
-            id: index, // id geven aan elk object in de array
+            id: index,
             cho: result.cho.value,
             title: result.title.value,
             materiaal: result.materiaalLabel.value,
@@ -55,68 +61,146 @@ async function fetchData() {
             land: result.landLabel.value
         }
     })
-    // Afwachten tot alle data is aangepast  en dan pas opslaan in 'results'
-    return await Promise.all(aangepasteDataArray)
 }
 
-// Filter-op-land functie
-function filterByCountry(dataArray, land) {
-    return dataArray.filter((item) => item.land == land)
+function amoutOfEachMaterial(readableData) {
+    const object = new Object()
+
+    readableData.forEach(dataElement => {
+        if (object[dataElement.materiaal] != null) {
+            object[dataElement.materiaal] += 1
+        } else {
+            object[dataElement.materiaal] = 1
+        }
+    })
+    return object
 }
 
-// IFFE
-(async () => {
-    const data = await fetchData()
-    console.log(filterByCountry(data, "Papua New Guinea"));
-})()
+//credits voor bald bitch a.k.a. wessel smit
+function fiveMostOccuringMaterials(allMaterials) {
+    const top5Materials = []
+
+    Object.keys(allMaterials).forEach(function (key) {
+        top5Materials.push(data = {
+            aantal: allMaterials[key],
+            materiaal: key.replace(key[0], key[0].toUpperCase())
+        })
+    })
+    return top5Materials.sort((lowest, highest) => highest.aantal - lowest.aantal).splice(0, 5)
+}
 
 
-// async function runQuery(endpoint, sparql) {
-//     const response = await fetch(endpoint + "?query=" + encodeURIComponent(sparql) + "&format=json");
-//     const data = await response.json();
-//     const rawData = data.results.bindings
-//     // console.log("Rawdata:", rawData)
+// var array = []
+// arrayWithNumber.forEach(number => {
+//     iets.push(number.aantal)
+// })
+// iets.sort(function (a, b) {
+//     return b - a
+// })
 
-//     return rawData
+// return array
 // }
 
-// function test() {
-//     const cleanArray = runQuery(endpoint, sparql).then(item => {
-//             return item.map((result, index) => {
-//                 return {
-//                     id: index,
-//                     cho: result.cho.value,
-//                     title: result.title.value,
-//                     materiaal: result.materiaalLabel.value,
-//                     type: result.voorwerpType.value,
-//                     land: result.landLabel.value
-//                 }
-//             })
-//         })
-//         .then(newData => {
-//             console.log("Remapped data:", newData)
-//         })
+
+// // //versie 1
+// function verzinNogEenNaam2(opgeschoondeData) {
+//     const emptyArray = []
+
+//     opgeschoondeData.forEach(element => {
+//         emptyArray.push(element.materiaal)
+//     })
+//     const uniqueItems = [...new Set(emptyArray)]
+
+//     console.log(uniqueItems)
 // }
-// test();
 
-/*
-async function filterByCountry(land) {
-    const dataArray = await fetchData()
+//versie 2
+// function verzinNogEenNaam2(opgeschoondeData) {
+//     return [...new Set(opgeschoondeData.map(result => {
+//         return {
+//             materiaal: result.materiaal
+//         }
+//     }))]
+// }
 
-    // dataArray.forEach(element => {
-    //     console.log(element.land)
-    // });
+//versie 3
+// function arrayUniekeMaterialen(opgeschoondeData) {
+//     return [...new Set(opgeschoondeData.map(voorwerp => voorwerp.materiaal))]
+// }
 
-    // Lange versie
-    // const newArray = dataArray.filter((item) => {
-    //     return item.land != land
-    // })
 
-    // Korte versie
-    const arraySameCountry = dataArray.filter((item) => item.land == land)
+// Welke types zijn er allemaal? En hoevaak komen deze voor in de array?
 
-    console.log("Aantal voorwerp in:", land, arraySameCountry)
-}
-*/
+// Maak een array met objecten van de types die het vaakst voorkomen (Top 5)
 
-// filterByCountry("Papua New Guinea")
+// Een berekening maken waarvan je de lengte van de categorie-array deelt door de lengte van de top 5 arrays
+
+// Deze waardes (procenten) toevoegen aan een nieuwe array die je gaat gebruiken in d3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function filterByCountry(dataArray, land) {
+//     return dataArray.filter((item) => item.land == land)
+
+//     // Landen om als voorbeeld te laten zien: 
+//     //  * Indonesia
+//     //  * Papua New Guinea
+// }
