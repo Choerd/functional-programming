294b8c4030cb3aef1afe92cd714a46ad333a4bbe
Sjors <sjorspieter@live.nl>
2019-11-14 22:19:28 +0100

    piechart-exercise

---
+++ piechart/modules/data.js
@@ -0,0 +1,45 @@
+const endpoint =
+    "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-24/sparql"
+
+const sparql = `
+PREFIX dc: <http://purl.org/dc/elements/1.1/>
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+PREFIX wgs84: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+PREFIX gn: <http://www.geonames.org/ontology#> 
+
+SELECT  ?cho
+        ?catName
+		?title
+		?materiaalLabel
+		?voorwerpType
+		?landLabel
+WHERE {
+<https://hdl.handle.net/20.500.11840/termmaster2809> skos:narrower ?cat . # keuze maken in categorie
+
+  # variabelen definiëren
+  ?cho edm:isRelatedTo ?cat . # ophalen van de link/uri van het voorwerp
+  ?cho dct:medium ?materiaal . # ophalen van het materiaal van het voorwerp
+  ?cho dc:type ?voorwerpType . # ophalen van het type van het voorwerp
+  ?cho dct:spatial ?plaats .
+
+  ?plaats skos:exactMatch/wgs84:lat ?lat . #
+  ?plaats skos:exactMatch/wgs84:long ?long .
+  ?plaats skos:exactMatch/gn:parentCountry ?land .
+
+  #Weergeven van de URI's in tekst
+  ?cho dc:title ?title . # titel van het voorwerp
+  ?cat skos:prefLabel ?catName .
+  ?land gn:name ?landLabel .
+  ?materiaal skos:prefLabel ?materiaalLabel .
+
+} ORDER BY DESC(?cho)
+`
+
+export default async function fetchData() {
+    const response = await fetch(endpoint + "?query=" + encodeURIComponent(sparql) + "&format=json")
+    const data = await response.json()
+
+    return data.results.bindings
+}
